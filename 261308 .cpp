c91c7914189cd4bfadab86bcf5ee9f8eb578246e
LeeYenTat <261308@hchs.edu.my>
2026-05-16 15:54:34 +0800

    Correct syntax and prompt for user name
    Fix syntax errors and add input for user name.

--- 261308 .cpp
+++ 261308 .cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string name ="Lee Yen Tat"
+int day=2, month=5, year=2013;
 
+cout << "Enter your name:";
+getline (cin,name);
+
+cout << "Hello" << name <<",your birthday is on" << day << "-" << month << "-" << year << ".";
+return 0;
+}
