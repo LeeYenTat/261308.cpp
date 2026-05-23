1313d6a79cfd3bec786fa4894aadb1a9e3575566
LeeYenTat <261308@hchs.edu.my>
2026-05-23 14:28:38 +0800

    Create    261308 .cpp

---
+++ 261308 .cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name, month;
+int day, year, age;
+
+//Input
+getline (cin, name);
+cin >> 2;
+cin >> May;
+cin >. 2013;
+
+//Calculate age
+age = 2025 - year;
+
+//Output
+cout << "Hello " << name << << ",your birthday is on " << day << "-" << month << "-" << year << ". You are currently " << age << " years old." ;
+return 0;
+}
